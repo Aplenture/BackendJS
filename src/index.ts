0dba0228d20eca709625f55650df6e372f7ebc09
fweise <public@weise-games.de>
2023-08-28 15:22:36 +0200

    implement fs utils

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,4 @@
 export * as Log from "./log";
 export * as Database from "./database";
 export * as Module from "./module";
+export * from "./utils";
---
+++ src/utils/fs.ts
@@ -0,0 +1,41 @@
+/**
+ * Aplenture/CoreJS
+ * https://github.com/Aplenture/CoreJS
+ * Copyright (c) 2023 Aplenture
+ * MIT License https://github.com/Aplenture/CoreJS/blob/main/LICENSE
+ */
+
+import * as FS from "fs";
+
+export interface LoadModuleConfig {
+    readonly class: string;
+    readonly path: string;
+}
+
+export function loadModule<T>(config: LoadModuleConfig, ...args: any[]): T {
+    const path = `${process.cwd()}/${config.path}.js`;
+
+    let constructor: new (...args: any[]) => T;
+
+    try {
+        constructor = require(path)[config.class];
+    } catch (error) {
+        throw new Error(`module '${config.class}' not found at '${path}'`);
+    }
+
+    return new constructor(...args);
+}
+
+export function loadConfig(name = 'config.json', _default = {}) {
+    const path = `${process.cwd()}/${name}`;
+
+    if (!FS.existsSync(path))
+        if (_default)
+            return _default;
+        else
+            throw new Error(`missing config at path '${path}'`);
+
+    const content = FS.readFileSync(path, 'utf8');
+
+    return JSON.parse(content);
+}
