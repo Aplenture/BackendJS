36bd59c2753a025b10972e93ea4ec1730b8d2341
fweise <public@weise-games.de>
2023-09-06 16:52:25 +0200

    clean up

--- src/balance/enums/updateType.ts
+++ src/balance/enums/updateType.ts
@@ -1,3 +1,10 @@
+/**
+ * Aplenture/BackendJS
+ * https://github.com/Aplenture/BackendJS
+ * Copyright (c) 2023 Aplenture
+ * MIT License https://github.com/Aplenture/BackendJS/blob/main/LICENSE
+ */
+
 export enum UpdateType {
     Increase = 1,
     Decrease,
